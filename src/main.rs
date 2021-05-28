aaaa94baacb26170909a86aa2f0c25453b20a6ef
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-05-28 06:37:17 +0200

    Work on card selection logic

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ extern crate termion;
 use rand::seq::SliceRandom;
 use std::collections::{HashMap, HashSet};
 use std::fmt;
+use std::io;
 
 const CARD_PAIRS: usize = 6;
 const CARDS_PER_ROW: usize = 4;
 // const ROWS: usize = (CARDS / CARDS_PER_ROW) + (CARDS % CARDS_PER_ROW).min(1);
+
+const SLEEP_MS: u64 = 1000;
 
 struct Card(pub usize);
 
@@ -31,53 +34,123 @@ impl fmt::Display for Card {
 
 type Pos = (usize, usize);
 
-type Grid = HashMap<Pos, Card>;
+struct Grid {
+    pub cards: HashMap<Pos, Card>,
+    pub size:  (usize, usize),
+}
 
-fn generate_grid() -> Grid {
-    let mut rng = rand::thread_rng();
+type GridSize = (usize, usize);
 
-    let mut shuffled_card_ids =
-        (0 .. CARD_PAIRS)
-            .into_iter()
-            .fold(Vec::new(), |mut pairs, i| {
-                pairs.push(i);
-                pairs.push(i);
-                pairs
-            });
-    shuffled_card_ids.shuffle(&mut rng);
-
-    let mut grid = HashMap::new();
-    let mut x = 0;
-    let mut y = 0;
-    for _ in 0 .. CARD_PAIRS * 2 {
-        if x >= CARDS_PER_ROW {
-            x = 0;
-            y += 1;
-        }
-
-        match shuffled_card_ids.pop() {
-            Some(id) => {
-                let pos = (x, y);
-                let card = Card(id);
-                grid.insert(pos, card);
-            }
-            None => {
-                panic!("Should have enough shuffled card ids for all cards")
-            }
-        }
-
-        x += 1;
+impl Grid {
+    pub fn new() -> Self {
+        Self::generate_grid()
     }
 
-    grid
+    fn generate_grid() -> Self {
+        let mut rng = rand::thread_rng();
+
+        let mut shuffled_card_ids =
+            (0 .. CARD_PAIRS)
+                .into_iter()
+                .fold(Vec::new(), |mut pairs, i| {
+                    pairs.push(i);
+                    pairs.push(i);
+                    pairs
+                });
+        shuffled_card_ids.shuffle(&mut rng);
+
+        let mut cards = HashMap::new();
+        let mut x = 0;
+        let mut y = 0;
+        let mut size = (0, 0);
+        for _ in 0 .. CARD_PAIRS * 2 {
+            if x >= CARDS_PER_ROW {
+                x = 0;
+                y += 1;
+            }
+
+            if x > size.0 {
+                size.0 = x
+            };
+            if y > size.1 {
+                size.1 = y
+            };
+
+            match shuffled_card_ids.pop() {
+                Some(id) => {
+                    let pos = (x, y);
+                    let card = Card(id);
+                    cards.insert(pos, card);
+                }
+                None => {
+                    panic!("Should have enough shuffled card ids for all cards")
+                }
+            }
+
+            x += 1;
+        }
+
+        Grid { cards, size }
+    }
 }
 
 fn main() {
-    let mut grid = generate_grid();
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    let mut grid = Grid::new();
 
     let mut is_running = true;
     let mut revealed: HashSet<Pos> = HashSet::new();
-    let mut selection: Option<Card> = None;
+    let mut selected: Vec<Pos> = Vec::with_capacity(2);
 
-    while is_running {}
+    while is_running {
+        if let Some(target) = get_input(&stdin) {
+            let is_on_grid = target.0 >= 0
+                && target.0 < grid.size.0
+                && target.1 >= 0
+                && target.1 < grid.size.1;
+            let is_revealed = revealed.contains(&target);
+            if is_on_grid && !is_revealed {
+                revealed.insert(target);
+                selected.push(target);
+            }
+        }
+
+        render(&mut stdout, &grid, &revealed);
+
+        if selected.len() == 2 {
+            if let (Some(card_a), Some(card_b)) =
+                (grid.cards.get(&selected[0]), grid.cards.get(&selected[1]))
+            {
+                let is_pair = card_a.0 == card_b.0;
+
+                if !is_pair {
+                    revealed.remove(&selected[0]);
+                    revealed.remove(&selected[1]);
+                }
+                selected.clear();
+            } else {
+                panic!("Selected cards aren't in grid");
+            }
+        }
+    }
+}
+
+fn get_input(stdin: &io::Stdin) -> Option<Pos> {
+    let mut input_buf = String::new();
+    let _ = stdin.read_line(&mut input_buf);
+    let input = input_buf.trim().replace(" ", "");
+    let parsed = input
+        .split("")
+        .filter_map(|s| s.parse::<usize>().ok())
+        .collect::<Vec<usize>>();
+    if parsed.len() == 2 {
+        Some((parsed[0], parsed[1]))
+    } else {
+        None
+    }
+}
+
+fn render(stdout: &mut io::Stdout, grid: &Grid, revealed: &HashSet<Pos>) {
 }
